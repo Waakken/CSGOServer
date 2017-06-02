a38c545a22d9220c86110a618cc7946899d4e64a
Jani Kuutvuori <janikuutvuori@hotmail.com>
2017-06-02 18:08:58 +0000

    Add scripts to start the game

---
+++ start/armsrace.sh
@@ -0,0 +1,1 @@
+/home/steam/Steam/games/csgo/srcds_run -game csgo -console -usercon +game_type 1 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount `cat TOKEN` +rcon_password "adminpassu" -tickrate 128
---
+++ start/deathmatch.sh
@@ -0,0 +1,1 @@
+/home/steam/Steam/games/csgo/srcds_run -game csgo -console -usercon +game_type 1 +game_mode 2 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount `cat TOKEN` +rcon_password "adminpassu" -tickrate 128

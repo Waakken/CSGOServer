e0056f1466655ebf103bae15c45a4ce19f8016f0
Your Name <you@example.com>
2017-07-05 01:03:21 -0400

    competitive script now works with fresh install

--- start/competitive.sh
+++ start/competitive.sh
@@ -1,2 +1,3 @@
 set -xeu
-/home/steam/Steam/games/csgo/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_cache +sv_setsteamaccount `cat TOKEN` +rcon_password "adminpassu" -tickrate 128 -maxplayers_override 20
+cd `cat ../INSTALL_DIR`/games/csgo/
+./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_cache +sv_setsteamaccount `cat TOKEN` +rcon_password "adminpassu" -tickrate 128 -maxplayers_override 20
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 set -xeu
-cd ../Steam
 ./steamcmd.sh +login anonymous +force_install_dir ./games/csgo +app_update 740 +quit
+pwd > INSTALL_DIR
+
